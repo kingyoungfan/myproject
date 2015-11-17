4552d7ffc156ebd7df016a874fe8e531833d281e
kingyoungfan <yangyang@yangyangdeMacBook-Pro.local>
2015-11-17 17:45:40 +0800

    update

--- myproject/index/index.py
+++ myproject/index/index.py
@@ -3,7 +3,7 @@ from pyramid.view import view_config
 __author__ = 'yangyang'
 
 
-@view_config(route_name='home.index', renderer='templates/index.html')
+@view_config(route_name='home.index', renderer='templates/index/index.html')
 def my_view(request):
     print('sdsd')
     return {'project': 'myproject'}
