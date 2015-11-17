32e1badd101b4186a9d389fe1fa4c5284f22a9d0
kingyoungfan <yangyang@yangyangdeMacBook-Pro.local>
2015-11-17 18:02:40 +0800

    update

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -11,7 +11,6 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
 
     routers.includeme(config)
